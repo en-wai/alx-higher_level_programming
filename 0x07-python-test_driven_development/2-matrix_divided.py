8dd9e0d7443b88fc77610624c189ebea2859ea7c
en-wai <nyasare@hotmail.com>
2023-02-02 20:43:28 -0800

    remove blank lines

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -45,20 +45,16 @@ def matrix_divided(matrix, div):
 
                         "integers/floats")
 
-
     if not all(len(row) == len(matrix[0]) for row in matrix):
 
         raise TypeError("Each row of the matrix must have the same size")
-
 
     if not isinstance(div, int) and not isinstance(div, float):
 
         raise TypeError("div must be a number")
 
-
     if div == 0:
 
         raise ZeroDivisionError("division by zero")
-
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
