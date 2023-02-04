615189c9b0d4b262587d3d8201036d18cfb6007b
en-wai <nyasare@hotmail.com>
2023-02-04 09:59:30 +0200

    Write a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def update_nary(a_dictionary, key, value):
 
+def update_dictionary(a_dictionary, key, value):
     """Replace or add key/value pairs in a dictionary."""
-
     a_dictionary[key] = value
-
     return (a_dictionary)
