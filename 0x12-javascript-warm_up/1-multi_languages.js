6fc484f7da4469bfd07b16835ac16f4927b3339c
en-wai <nyasare@hotmail.com>
2023-04-12 00:37:54 +0000

    Add soln to task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,6 @@
 let line1 = "C is fun";
 let line2 = "Python is cool";
 let line3 = "JavaScript is amazing";
-console.log(line1\nline2\nline3);
+console.log(line1);
+console.log(line2);
+console.log(line3);
