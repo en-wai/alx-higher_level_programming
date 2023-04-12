48d02a4945fff59cdb38129f3cde38ca782140f9
en-wai <nyasare@hotmail.com>
2023-04-12 14:56:33 +0000

    Add Task 9. logme

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -5,11 +5,11 @@ Prototype: exports.nbOccurences = function (list, searchElement)
 */
 
 exports.nbOccurences = function (list, searchElement) {
-  let num_occ = 0;
+  let numOcc = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      num_occ += 1;
+      numOcc += 1;
     }
   }
-  return num_occ;
+  return numOcc;
 };
