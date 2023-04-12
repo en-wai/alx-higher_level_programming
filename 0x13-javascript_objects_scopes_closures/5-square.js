426bdf94bebef05e2fa2a07f121e8cc864e29b2c
en-wai <nyasare@hotmail.com>
2023-04-12 14:22:18 +0000

    Add Task 5. Rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-/*  
+/*
 Write a class Square that defines a square and inherits from Rectangle of 4-rectangle.js:
 
 You must use the class notation for defining your class and extends
