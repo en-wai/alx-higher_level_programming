7c36c4d3f759f4741d0ca3349b2a4018f55d961c
en-wai <nyasare@hotmail.com>
2023-04-12 01:29:43 +0000

    Add Soln to task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/* print My number: <first argument converted in integer>
+if 1st argument can be converted to an integer else print given conditions*/
+const arg = parseInt(process.argv[2]);
+if (Number.isNaN(arg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + arg);
+}
