896e46335822237f536a96e05986fad97251c101
en-wai <nyasare@hotmail.com>
2023-04-12 01:36:13 +0000

    Add Soln to task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* Create script that prints 3 lines: (like 1-multi_languages.js)
 by using an array of string and a loop */
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+const languageArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < languageArray.length; i++) {
+  console.log(languageArray[i]);
 }
