ec3e274370e096246a8867315f15a5bd04c7f890
en-wai <nyasare@hotmail.com>
2023-04-12 13:21:23 +0000

    Add Task 0

---
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/** Write an empty class Rectangle that defines a rectangle:
+ *You must use the class notation for defining your class
+*/
+const Rectangle = require('./0-rectangle');
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
