82ba30fcabb484403954563706150f957127bf0a
en-wai <nyasare@hotmail.com>
2023-04-12 02:15:46 +0000

    Add Soln to task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Write code that computes and prints a factorial
 const argvar = parseInt(process.argv[2]);
-function Factorial (n) {
+function Factorial(n) {
   if ((Number.isNaN(n)) || (n === 1)) {
     return 1;
   }
