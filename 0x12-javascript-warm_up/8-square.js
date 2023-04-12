60669c6ec9b389df62dc382ac442a6bcc19ae00b
en-wai <nyasare@hotmail.com>
2023-04-12 01:47:10 +0000

    Add Soln to task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // Write code that prints a square.
-const sq_size = process.argv[2];
-if (isNaN(sq_size)) {
+const squareSize = process.argv[2];
+if (isNaN(squareSize)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < sq_size; i++) {
-    console.log('X'.repeat(sq_size));
+  for (let i = 0; i < squareSize; i++) {
+    console.log('X'.repeat(squareSize));
   }
 }
